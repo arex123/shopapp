d3f8e093430065d32e518b180926683c56e52bd4
arex123 <ad47kumar@gmail.com>
2024-08-03 22:48:22 +0530

    model

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,5 @@
 const products = []
+const Product = require('../models/products')
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {
@@ -12,19 +13,23 @@ exports.getAddProduct = (req, res, next) => {
 
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    // products.push({ title: req.body.title });
+    let prod = new Product(req.body.title)
+    prod.save()
     res.redirect('/');
 }
 
 exports.getProducts =  (req, res, next) => {
-    res.render('shop', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
-    });
+    Product.fetchAll(products=>{
+      res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+      });
+  })
 }
 
 exports.contactup = (req,res)=>{
